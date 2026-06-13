f1bd7e8f49575b3b5811845cee12e24154c2681d
undergroundr97 <123432660+undergroundr97@users.noreply.github.com>
2026-06-12 22:47:57 -0300

    feat: adiciona arquivos projeto base

---
+++ src/main/java/com/courses/ocourses/OcoursesApplication.java
@@ -0,0 +1,13 @@
+package com.courses.ocourses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcoursesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcoursesApplication.class, args);
+	}
+
+}
